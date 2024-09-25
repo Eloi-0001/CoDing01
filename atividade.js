595957ff7c46e6c796f4e851d498a34a1a84be65
Eloi-0001 <lucaseloideolibeira@gmail.com>
2024-09-25 10:39:48 -0300

    criaei um repositório mt top

---
+++ atividade.js
@@ -0,0 +1,68 @@
+var prompt = require('prompt-sync')();
+
+ /* programa 1 
+
+    var nome = ("Eloi")
+
+    var idade = (19)
+
+ /* programa 2 
+
+    var cidade = ('recife')
+
+    console.log ("Olá, meu nome é " + nome +" e tenho "+ idade +" anos.");
+
+    console.log(`${nome} é de ${cidade}`)
+
+ /* programa 3 
+
+    let numero = prompt("Digite um numero")
+
+    console.log(numero + numero)
+    console.log(numero - numero)
+    console.log(numero * numero)
+    console.log(numero / numero)
+
+ /* programa 4 
+
+    let base = prompt("Digite base")
+    let altura = prompt("Digite altura")
+
+    let mult = (base * altura)
+    console.log(`O resultado da multiplicação é ${mult}`)
+
+    let div = (mult / 2)
+    console.log(`O resulto da média é ${(div)}`)
+
+ /* programa 5 
+
+    var nota1 = parseFloat(prompt('digiti nota 1'));
+    var nota2 = parseFloat(prompt('digiti nota 2'));
+    var nota3 = parseFloat(prompt('digiti nota 3'));
+
+    var sm = (nota1 + nota2 + nota3)
+    var cm = (sm / 3)
+    console.log(`O calculo da media é ${sm}`)
+    console.log(`O resultado da sua media é ${cm.toFixed(1)}`)
+
+ /* programa 6 
+
+    let preco = parseFloat(prompt('Valor do produto é?'))
+    let precoD = parseFloat(prompt('Valor do desconto é?'))
+
+    let desconto = (preco * precoD) / 100;
+    var precoF = preco - desconto;
+
+    console.log(`Seu preço final é ${precoF}R$`)
+
+ /* programa 7
+
+ let renda = parseFloat(prompt('qual sua renda mensal?'))
+ var imposto = 10
+
+ var impostoRenda = (renda*imposto)/100;
+
+console.log(`seu valor a ser pago é ${impostoRenda}R$`);
+
+ /* programa 7 */ 
+ 
