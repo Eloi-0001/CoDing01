6123f562e8397bdde9e6db4a183440d6adcfb48d
Kacaii <kacaii.privado@gmail.com>
2024-10-23 19:24:45 -0300

    Add TODO comment tags

--- atividades/Carros_4_the_movie.js
+++ atividades/Carros_4_the_movie.js
@@ -3,6 +3,7 @@ const carros = ["Fusca ", "Civic ", "Corolla ", "Mustang ", "Onix"];
 
 console.log(`\nOlá cliente!!, nós temos esses carros:\n`); // Insere uma nova linha na tela
 
+// Coloquei um `foreach()` aqui | @Kacaii
 carros.forEach((carro, index) => {
   console.log(`${index}: ${carro}`); // Exibe a lista de carros na tela
 });
@@ -11,16 +12,20 @@ const desejaComprar = prompt(
   "\nVocê deseja comprar algum objeto da lista?\n (Sim ou Não) \n\n>",
 ).toLocaleLowerCase();
 
+// HACK: Talvez tenha um jeito melhor de escrever essa funcionalidade (?).
 if (desejaComprar === "sim") {
+  // WARN: Falta escrever o codigo dessa função
   const escolhaCarro = parseInt(
     prompt("qual o numero do carro que você quer alterar?"),
   );
+  // WARN: Falta escrever o codigo dessa função
   const removerCarro = parseInt(
     prompt("qual o numero do carro você deseja remover?"),
   );
 
   const novoCarro = prompt("novo carro");
 
+  // WARN: Variável não usada
   const atualizandoCarro = carros.splice(escolhaObj, removerObj, novoCarro);
 
   const numeroCarros = carros.length;
@@ -30,6 +35,8 @@ if (desejaComprar === "sim") {
   );
 } else {
   const novoCarro = prompt("novo carro");
+
+  // WARN: Variável não usada
   const atualizandoCarro = carros.splice(escolhaObj, 0, novoCarro);
 
   const numeroDeCarros = carros.length;
